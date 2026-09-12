526b6e8980a95d8626007a66ca2eeac4754a8564
MorastGit <morastsys@gmail.com>
2026-09-12 09:48:29 +0300

    add pop on new branch

---
+++ Practice/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Practice;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("pop");
+        Console.WriteLine("pop");
+        Console.WriteLine("pop");
+        Console.WriteLine("pop");
+    }
+}
